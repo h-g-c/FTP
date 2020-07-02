760b893702fba351caa5b8cb01f168034717910c
haolvh <2569551290@qq.com>
2020-07-02 11:32:46 +0800

    create clientGUI

---
+++ src/main/java/client/Hello.java
@@ -0,0 +1,12 @@
+package client;
+
+/**
+ * @author LvHao
+ * @Description hello
+ * @date 2020-07-02 11:30
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("begin");
+    }
+}
