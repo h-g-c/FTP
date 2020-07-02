5332fec40aa57bee4744277fd258b8fb2d2973a4
Chao-Yin-Github <877891173@qq.com>
2020-07-02 10:51:46 +0800

    feat: add lombok dependencies and add protocol

---
+++ src/main/java/util/Protocol.java
@@ -0,0 +1,32 @@
+package util;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 客户端和服务端通信协议
+ * @author yinchao
+ * @date 2020/7/2 10:37
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Protocol implements Serializable {
+    // 目标ip
+    String targetIp;
+    // 发送方ip
+    String sourceIp;
+    // 传输类型
+    TransmissionType transmissionType;
+    // 传输信息
+    String message;
+    // 命令端口号
+    Integer CommandPort;
+    // 数据传输端口号
+    Integer DataPort;
+}
